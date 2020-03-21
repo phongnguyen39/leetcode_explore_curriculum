ed2c94afd86362777af27e797386cfce05b94039
phongnguyen39 <phong.nguyen39@gmail.com>
2020-03-21 11:39:03 -0700

    deleted junk file

--- delete.js
+++
@@ -1,8 +0,0 @@
-let arr = [1,2,3,4]
-
-let sum = arr.reduce((a,b)=> a+b)
-sum1 = arr.reduce((a,b)=> a+b)
-sum2 = arr.reduce((a,b)=> a+b)
-sum3 = arr.reduce((a,b)=> a+b)
-
-console.log(sum, sum1, sum2, sum3)
